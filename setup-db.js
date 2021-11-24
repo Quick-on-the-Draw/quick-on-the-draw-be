465075712dd61790317a850a3238f747aa82da45
kb <katrinkjb@gmail.com>
2021-11-23 17:10:49 -0800

    fixed deployment issue by updating setup steps and db url

--- setup-db.js
+++ setup-db.js
@@ -1,6 +1,9 @@
 const pool = require('./lib/utils/pool.js');
 const setup = require('./data/setup.js');
+const seedTables = require('./data/load-seed-data.js');
 
 setup(pool)
     .catch((err) => console.error(err))
     .finally(() => process.exit());
+
+seedTables();
